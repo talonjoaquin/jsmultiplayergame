4db0555fe13042138a8d40bdb6072c6a78ee8703
Joaquin <jtalon@stevens.edu>
2019-06-04 01:37:21 -0400

    added wandering for npcs

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var ais = {};
 var playerspeed = 0.1;
 var npspeed = 0.05;
 var NPCLIM = 100;
+var aggroRange = 200000;
 
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
@@ -29,7 +30,9 @@ server.listen(5000, function(){
 for (var i = 0; i < NPCLIM; i++){
     ais[i] = {
         x: Math.floor(Math.random() * 1400),
-        y: Math.floor(Math.random() * 900)
+        y: Math.floor(Math.random() * 900),
+        wanderlust: 0,
+        wanderang: 0
     }
 }
 
@@ -73,11 +76,27 @@ setInterval(function(){
                 chasedPlayer = player;
             }
         }
-        if(chasedPlayer != undefined){
+        if(chasedPlayer != undefined && distToPlayer < aggroRange){
+            npc.wanderlust = 0;
             var angleToPlayer = Math.atan2((chasedPlayer.y - npc.y), (chasedPlayer.x - npc.x));
-        
-            npc.x += Math.cos(angleToPlayer) * npspeed * timeDifference;
-            npc.y += Math.sin(angleToPlayer) * npspeed * timeDifference;
+            var proximitySpeedCoeff = Math.max(0, (aggroRange - distToPlayer)/aggroRange);
+            npc.x += Math.cos(angleToPlayer) * npspeed * timeDifference * proximitySpeedCoeff;
+            npc.y += Math.sin(angleToPlayer) * npspeed * timeDifference * proximitySpeedCoeff;
+        }else{
+            if(npc.wanderlust > 0){
+                npc.wanderlust--;
+                npc.x += Math.cos(npc.wanderang) * npspeed * timeDifference * 0.25;
+                npc.y += Math.sin(npc.wanderang) * npspeed * timeDifference * 0.25;
+                if(npc.x > 1400 || npc.x < 0){
+                    npc.wanderang = Math.PI - npc.wanderang;
+                }
+                if(npc.y > 900 || npc.y < 0){
+                    npc.wanderang = Math.PI * 2 - npc.wanderang;
+                }
+            }else{
+                npc.wanderlust = Math.floor(Math.random() * 1000) + 100;
+                npc.wanderang = Math.random() * 2 * Math.PI;  
+            }
         }
     }
     for (var id in players){
