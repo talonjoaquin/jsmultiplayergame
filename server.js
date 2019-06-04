7f3d7eb9d310ac80c4cca14cebaf9493db0bfa33
Joaquin <jtalon@stevens.edu>
2019-06-04 01:26:14 -0400

    added npc movement

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var players = {};
 var ais = {};
 
 var playerspeed = 0.1;
-var NPCLIM = 10;
+var npspeed = 0.05;
+var NPCLIM = 100;
 
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
@@ -60,7 +61,25 @@ setInterval(function(){
     //update state
     var currentTime = (new Date()).getTime(); 
     var timeDifference = currentTime - lastUpdateTime;
-
+    for(var id in ais){
+        var npc = ais[id];
+        var distToPlayer = Infinity;
+        var chasedPlayer = undefined;
+        for(var pid in players){
+            var player = players[pid];
+            var tempDist = (player.x - npc.x)*(player.x - npc.x)+(player.y - npc.y)*(player.y - npc.y);
+            if(tempDist < distToPlayer){
+                distToPlayer = tempDist;
+                chasedPlayer = player;
+            }
+        }
+        if(chasedPlayer != undefined){
+            var angleToPlayer = Math.atan2((chasedPlayer.y - npc.y), (chasedPlayer.x - npc.x));
+        
+            npc.x += Math.cos(angleToPlayer) * npspeed * timeDifference;
+            npc.y += Math.sin(angleToPlayer) * npspeed * timeDifference;
+        }
+    }
     for (var id in players){
         var player = players[id];
         if(player.left){
